5d42f7df5bf0b02009a4aa8aa17c17849d39b6c5
Gapi-gap <156783799+Gapi-gap@users.noreply.github.com>
2025-08-31 20:44:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BPI service = new BPI();
-        double Height = 1.87;
-        double Massa = 98;
-        int result = service.calculate(Massa, Height); // должно получиться 500
+        double height = 1.87;
+        double weight = 98;
+        int result = service.calculate(weight, Height); // должно получиться 500
         System.out.println(result);
     }
 }
