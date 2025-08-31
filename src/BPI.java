bc953ba4effd97899832c3be484af5523eeb1f6e
artem <artem.gaponov.2024@mail.ru>
2025-08-31 20:34:29 +0300

    Отформатирован кол

--- src/BPI.java
+++ src/BPI.java
@@ -1,7 +1,7 @@
 public class BPI {
-    int calculate(double massa, double height){
-        double bpi =  massa/(height*height);
-        int result = (int)bpi;
-        return (int)result;
+    int calculate(double massa, double height) {
+        double Bpi = massa / (height * height);
+        int Result = (int) Bpi;
+        return (int) Result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BPI service = new BPI();
-        double height = 1.87;
-        double massa = 98;
-        int result = service.calculate(massa, height); // должно получиться 500
+        double Height = 1.87;
+        double Massa = 98;
+        int result = service.calculate(Massa, Height); // должно получиться 500
         System.out.println(result);
     }
 }
